9e9506643f0544435b8a1d202b0b852825aae113
Kyle Bye <kylebye4mav@gmail.com>
2022-10-31 19:35:29 -0500

    stuff

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const expressHandlebars = require('express-handlebars')
 const app = express();
 
 app.use(express.static(__dirname + '/views'));
+app.use(express.urlencoded({
+	extended: true
+}))
 app.engine('handlebars', expressHandlebars.engine({
 	defaultLayout: 'main',
   }));
@@ -25,10 +28,27 @@ app.get('/about',  function(request, response) {
 });
 
 app.get('/test', function(request, response) {
-	response.type('text/plain')
-	response.send('Node.js and Express running on port='+port)
+	response.type('text/plain');
+	response.send('Node.js and Express running on port='+port);
+});
+
+app.post('/processlogin', function(request, response) {
+
+	//	Form inputs
+	const username = request.body.username;
+	const password = request.body.password;
+
+	// Right now, I made the response to display what you entered
+	// on the login page.
+	let responseMsg = "So... this is what we got on the backend...\n";
+	responseMsg += "Username: " + username + "\n";
+	responseMsg += "Password: " + password + "\n";
+
+	response.type('text/plain');
+	response.send(responseMsg);
+
 });
 
 app.listen(port, function() {
-	console.log("Server is running at http://localhost:3000/")
+	console.log("Server is running at http://localhost:3000/");
 });
