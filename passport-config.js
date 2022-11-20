9eb834330c9be2e4beaa7ffce13cd643494320cb
Kyle Bye <kylebye4mav@gmail.com>
2022-11-20 02:27:45 -0600

    bcryptjs replacement

--- passport-config.js
+++ passport-config.js
@@ -1,5 +1,5 @@
 const LocalStrategy = require('passport-local').Strategy;
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 function init(passport, getUserByUsername, getUserByID) {
 
